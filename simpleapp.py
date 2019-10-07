e4a9b8d4d0b8808295c45275f3adf136601b1343
sha-net <shavit.netzer@gmail.com>
2019-10-07 23:26:50 +0300

    adding build image

--- simpleapp.py
+++ simpleapp.py
@@ -10,7 +10,7 @@ start = int(round(time.time()))
 @app.route("/")
 def hello_world():
 
-    return "Version 2"
+    return "Version 1"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
