6ca41ce04284ed183e88e0f388482cfc9c866cf6
sha-net <shavit.netzer@gmail.com>
2019-10-11 15:21:11 +0300

    adding build image

--- template-script.sh
+++ template-script.sh
@@ -4,21 +4,21 @@ if [ $# -eq 0 ]
     echo "No arguments supplied, usage ./$0 APP-NAME APP-PORT IMAGE"
     exit 0
 else
-TEMPLATE-APP-NAME=$2
-TEMPLATE-APP-PORT=$3
-TEMPLATE-IMAGE=$4
-YAML-FILE="pod-svc.yaml"
+TEMPLATE_APP_NAME="$2"
+TEMPLATE_APP_PORT="$3"
+TEMPLATE_IMAGE="$4"
+YAML_FILE="pod-svc.yaml"
 TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
 case "$1" in
         start)
-            sed -i "s/TEMPLATE-APP-NAME/$TEMPLATE-APP-NAME/g" $YAML-FILE
-            sed -i "s/TEMPLATE-APP-PORT/$TEMPLATE-APP-PORT/g" $YAML-FILE
-            sed -i "s/TEMPLATE-IMAGE/$TEMPLATE-IMAGE/g" $YAML-FILE
-            curl -q -k -X POST https:///$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/jenkins/pods --header "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d@$YAML-FILE
+            sed -i "s/TEMPLATE_APP-NAME/$TEMPLATE_APP_NAME/g" $YAML_FILE
+            sed -i "s/TEMPLATE_APP-PORT/$TEMPLATE_APP_PORT/g" $YAML_FILE
+            sed -i "s/TEMPLATE_IMAGE/$TEMPLATE_IMAGE/g" $YAML_FILE
+            curl -q -k -X POST https:///$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/jenkins/pods --header "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d@$YAML_FILE
             ;;
          
         stop)
-            curl -q -k -X GET https:///$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/jenkins/pods/$TEMPLATE-APP-NAME --header "Authorization: Bearer $TOKEN"
+            curl -q -k -X GET https:///$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/jenkins/pods/$TEMPLATE_APP_NAME --header "Authorization: Bearer $TOKEN"
             ;;
          
         *)
