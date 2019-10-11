61b6f12c5c1416b34d6dbd9589c04d8775fd5e33
sha-net <shavit.netzer@gmail.com>
2019-10-11 22:04:10 +0300

    adding build image

--- simpleapp.py
+++ simpleapp.py
@@ -1,22 +1,26 @@
-from flask import Flask
-import sys
-import optparse
-import time
+    #!flask/bin/python
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-start = int(round(time.time()))
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
 
-@app.route("/")
-def hello_world():
-
-    return "Version 2"
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser(usage="python simpleapp.py -p ")
-    parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen on.')
-    (args, _) = parser.parse_args()
-    if args.port == None:
-        print "Missing required argument: -p/--port"
-        sys.exit(1)
     app.run(host='0.0.0.0', port=int(args.port), debug=False)
