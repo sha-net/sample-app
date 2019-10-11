b356989d115ca596421d353c04166a0c868bbbd2
sha-net <shavit.netzer@gmail.com>
2019-10-11 15:34:08 +0300

    adding build image

--- template-script.sh
+++ template-script.sh
@@ -11,9 +11,9 @@ YAML_FILE="pod-svc.yaml"
 TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
 case "$1" in
         start)
-            sed -i "s/TEMPLATE_APP-NAME/$TEMPLATE-APP-NAME/g" $YAML_FILE
-            sed -i "s/TEMPLATE_APP-PORT/$TEMPLATE-APP-PORT/g" $YAML_FILE
-            sed -i "s/TEMPLATE_IMAGE/$TEMPLATE-IMAGE/g" $YAML_FILE
+            sed -i "s/TEMPLATE-APP-NAME/$TEMPLATE_APP_NAME/g" $YAML_FILE
+            sed -i "s/TEMPLATE-APP-PORT/$TEMPLATE_APP_PORT/g" $YAML_FILE
+            sed -i "s/TEMPLATE-IMAGE/$TEMPLATE_IMAGE/g" $YAML_FILE
             curl -q -k -X POST https:///$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/jenkins/pods --header "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d@$YAML_FILE
             ;;
          
