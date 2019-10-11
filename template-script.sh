ed4d8fe9bf276035e963d136bb89970ec2fddbeb
sha-net <shavit.netzer@gmail.com>
2019-10-11 20:27:32 +0300

    adding build image

--- template-script.sh
+++ template-script.sh
@@ -44,10 +44,12 @@ spec:
 EOF
 case "$1" in
         start)
+            echo curl -q -k -X POST https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/$NAMESPACE/pods --header "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d@$YAML_FILE >> log.txt
             curl -q -k -X POST https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/$NAMESPACE/pods --header "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" -d@$YAML_FILE
             ;;
          
         stop)
+            echo curl -q -k -X GET https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/$NAMESPACE/pods/$TEMPLATE_APP_NAME --header "Authorization: Bearer $TOKEN" >> log.txt
             curl -q -k -X GET https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/$NAMESPACE/pods/$TEMPLATE_APP_NAME --header "Authorization: Bearer $TOKEN"
             ;;
          
